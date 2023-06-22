16aa92a392b8e1d6be833db630c54ea7fd856124
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-22 04:24:34 +0000

    AirBnB clone - MySQL

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,8 @@
--- prepares a MySQL server
--- for development of the AirBnB clone project
+-- Creates a database hbnb_dev_db
+-- Adds a new user hbnb_dev (in localhost)
+-- Sets the password of hbnb_dev should be set to hbnb_dev_pwd
+-- Grants hbnb_dev all privileges on the database hbnb_dev_db
+-- Grants hbnb_dev SELECT privilege on the database performance_schema
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
