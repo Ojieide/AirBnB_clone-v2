cfa3bc1c20500644c14f09b6075011cee55393df
Ojieide Egbomondion <ojieide@yahoo.com>
2023-07-06 15:43:52 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,26 +3,32 @@
 Fabric script that distributes an archive to your web servers,
 using the function do_deploy
 """
+from fabric.contrib import files
+from fabric.api import env, put, run
 import os
-from fabric.api import run, env, cd, put
+
 env.hosts = ['52.201.190.14', '54.160.65.122']
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
     """ Distributes an archive to your web servers """
-    if (os.path.exists(archive_path) is False):
+    if not os.path.exists(archive_path):
         return False
-    filename = '/data/web_static/releases/{}'.format(
-        archive_path.strip(".tgz"))
-    archive = archive_path.lstrip('versions/')
-    run('mkdir -p {}'.format(filename))
-    # run('mkdir /tmp/versions')
-    put('{}'.format(archive_path), "/tmp/")
-    run('tar -xzf /tmp/{} -C {}'.format(archive, filename))
-    run('mv {}/web_static/* {}'.format(filename, filename))
-    run('rm -rf {}/web_static/'.format(filename))
-    run('rm /tmp/{}'.format(archive))
-    run('rm /data/web_static/current')
-    run('ln -sf {} /data/web_static/current'.format(filename))
-    return True
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
+    try:
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except Exception:
+        return False
